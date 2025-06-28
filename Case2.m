ffee6e7a83e83674f485fcfa07110af287c000c4
WangZhuning <44920305+WangZhuning@users.noreply.github.com>
2025-06-28 22:12:07 +0800

    Add files via upload

---
+++ Case2.m
@@ -0,0 +1,25 @@
+%%%%%%%%%%%%%%
+%Case 2:Steady-state temperature of blackbody emitter
+%%%%%%%%%%%%%%
+    clear;close all;
+%%%%%%%%%%%%%%%%
+%Parameters Setting
+%%%%%%%%%%%%%%%%%
+    Tamb=300;   %ambient temperature,(K)
+    tau_nir=0;  %infrared transmittance
+    r_nir=0;    %infrared reflectance
+    tau_solar=0;%solar transmittance
+    r_solar=1;  %solar reflectance
+    S=1000;     %solar irradiance(W/m2)
+    cloud=0;    %cloud cover
+    h=0;        %Non-radiative heat transfer coefficient(W/m2)
+%%%%%%%%%%%%%%%%
+%Steady-state temperature Calculating
+%%%%%%%%%%%%%%%%%
+    Temperature=Tcool_cal(Tamb,tau_nir,r_nir,tau_solar,r_solar,S,cloud,h);
+%%%%%%%%%%%%%%
+%Result output
+    disp(["Steady-state temperature: "+num2str(Temperature-273.15)+"°C"]);
+
+
+    
---
+++ Tcool_cal.m
@@ -0,0 +1,23 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   Calculation of steady-state temperature for solid planar emitters
+%   Parameters:
+%           Tamb:       ambient temperature,(K)
+%           Temperature (K)
+%           tau_nir:    infrared transmittance
+%           r_nir:      infrared reflectance
+%           tau_solar:  solar transmittance
+%           r_solar:    solar reflectance
+%           S:          solar irradiance
+%           cloud:      cloud cover
+%           h:          Non-radiative heat transfer coefficient(W/m2)
+
+%   Result: Temperature steady-state temperature(K),when cooling power==0
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function Temperature=Tcool_cal(Tamb,tau_nir,r_nir,tau_solar,r_solar,S,cloud,h)
+%%calculating
+syms Tunknown
+eqn=Pcool_cal(Tamb,Tunknown,tau_nir,r_nir,tau_solar,r_solar,S,cloud,h)==0;
+sol=vpasolve(eqn,Tunknown,'Random',true);
+Temperature=double(sol(sol==real(sol)&sol>0));
+Temperature=Temperature(1);
+end
